69010f3d01142b8614f85ea456f92058525b7b32
ryan2625 <ryanfreas19@augustana.edu>
2023-03-14 00:42:05 -0500

    editing dashboard layout

--- src/components/Dashboard/DashBoard.js
+++ src/components/Dashboard/DashBoard.js
@@ -38,6 +38,12 @@ function Dashboard() {
                     <div className="dashboard-container">
                     <div className="graph">Graph with user data</div>
                     <div className="inspirational">You can do it!</div>
+                    <div className="inspirational">
+                        <h1>BMI calculator</h1>
+                        <input type="text" style={{color: "white"}} placeholder="Enter your height in Inches"></input>
+                        <input type="text" style={{color: "white"}} placeholder="Enter your weight in Inches"></input>
+                        <h3>BMI</h3>
+                    </div>
                     </div>
                 </div>
         
