eab3c04558a68eac2f8187b8b6bea1dfc6d2ff01
ryan2625 <ryanfreas19@augustana.edu>
2023-03-15 04:18:32 -0500

    bmi calculator

--- src/components/Dashboard/dashboardComponent/dashboardMain/dashboardMain.js
+++ src/components/Dashboard/dashboardComponent/dashboardMain/dashboardMain.js
@@ -2,6 +2,23 @@ import "./dashboardMain.css"
 import React, { useEffect, useState } from "react";
 
 function DashboardMain(){
+
+    const [height,setHeight]=useState(0)
+    const [weight,setWeight]=useState(0)
+    const [bmi, setBMI]=useState(0)
+
+    function setWeightFun(val){
+        setWeight(val.target.value)
+    }
+
+    function setHeightFun(val){
+        setHeight(val.target.value)
+    }
+
+    function calcBMI() {
+        setBMI((weight / (height*height) * 703))
+    }
+
     return(
         <div className="dashboard">
                     <div className="dashboard-container">
@@ -9,9 +26,10 @@ function DashboardMain(){
                     <div className="inspirational">You can do it!</div>
                     <div className="inspirational">
                         <h1>BMI calculator</h1>
-                        <input type="text" style={{color: "white"}} placeholder="Enter your height in Inches"></input>
-                        <input type="text" style={{color: "white"}} placeholder="Enter your weight in Inches"></input>
-                        <h3>BMI</h3>
+                        <input type="text" style={{color: "white"}} placeholder="Enter your height in Inches" onChange={setHeightFun}></input>
+                        <input type="text" style={{color: "white"}} placeholder="Enter your weight in Inches" onChange={setWeightFun}></input>
+                        <button onClick={calcBMI}>Submit</button>
+                        <h3>BMI: {bmi}</h3>
                     </div>
                     </div>   
                 </div>
