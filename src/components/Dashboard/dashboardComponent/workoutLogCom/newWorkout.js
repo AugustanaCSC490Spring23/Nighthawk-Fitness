bfb7c9bff2a79454cb9457ba5ace6efa8228c572
ryan2625 <ryanfreas19@augustana.edu>
2023-03-19 04:28:17 -0500

    updating feature for submitting form

--- src/components/Dashboard/dashboardComponent/workoutLogCom/newWorkout.js
+++ src/components/Dashboard/dashboardComponent/workoutLogCom/newWorkout.js
@@ -1,15 +1,52 @@
-import "./workoutLog.css"
-function NewWorkout(){
+import "./newWorkout.css"
+import React, { useEffect, useState } from "react";
+
+const NewWorkout = () => {
+    const[workoutInfo, setWorkoutInfo] = useState({
+        workoutName: "",
+        weight: 0,
+        reps: 0,
+    })
+
+    const handleChange = (event) =>{
+        setWorkoutInfo({...workoutInfo, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        console.log(workoutInfo)
+    }
+
     return <>
     <h1>Add a new Workout</h1>
-    <form>
+    <div className="formWorkout">
+    <form onSubmit={handleSubmit}>
         <label>Workout</label>
-        <input type="text" required></input>
+        <input 
+            type="text" 
+            name="workoutName"
+            value={workoutInfo.workoutName}
+            onChange={handleChange}
+            required>
+        </input>
         <label>Weight</label>
-        <input type="text" required></input>
+        <input 
+            type="text" 
+            name="weight"
+            value={workoutInfo.weight}
+            onChange={handleChange}
+            required>
+        </input>
         <label>How many Rep</label>
-        <input type="text" required></input>
+        <input type="text" 
+        name="reps"
+        value={workoutInfo.reps}
+        onChange={handleChange}
+        required>
+        </input>
+        <button>Create workout!</button>
     </form>
+    </div>
     </>
 }
 
--- src/components/Dashboard/dashboardComponent/workoutLogCom/workoutLog.js
+++ src/components/Dashboard/dashboardComponent/workoutLogCom/workoutLog.js
@@ -1,12 +1,17 @@
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import { Button, Icon, IconButton } from '@mui/material';
+import NewWorkout from './newWorkout';
+import React, { useEffect, useState } from "react";
 import './workoutLog.css'
 function WorkoutLog() {
+  const [added, addWorkout] = useState(false)
     return(
         <div className='log-container'>
-        <IconButton id="add-btn" aria-label="Log a workout">
+        <IconButton id="add-btn" aria-label="Log a workout" 
+        onClick={()=>addWorkout(!added)}>
           <AddCircleOutlineIcon/>
         </IconButton>
+        {added && <NewWorkout/>}
         </div>
     )
 }
