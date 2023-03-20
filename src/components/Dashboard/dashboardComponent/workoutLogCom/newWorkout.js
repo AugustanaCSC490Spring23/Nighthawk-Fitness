e5d384ca0e2cbec902f2bf8617f81e57d8d13976
ryan2625 <ryanfreas19@augustana.edu>
2023-03-20 17:13:47 -0500

    i handle submit form XD

--- src/components/Dashboard/dashboardComponent/workoutLogCom/newWorkout.js
+++ src/components/Dashboard/dashboardComponent/workoutLogCom/newWorkout.js
@@ -1,63 +1,77 @@
 import "./newWorkout.css"
 import React, { useEffect, useState } from "react";
+import { useForm } from 'react-hook-form'
 
 const NewWorkout = () => {
+
+    class Workouts{
+        constructor(workoutName, workoutCount, weight,
+            reps){
+                this.workoutName = workoutName
+                this.workoutCount = workoutCount
+                this.weight = weight
+                this.reps = reps
+            }
+    }
     const[workoutInfo, setWorkoutInfo] = useState({
         workoutName: "",
-        weight: 0,
-        reps: 0,
+        weight: "",
+        reps: "",
     })
 
     const handleChange = (event) =>{
         setWorkoutInfo({...workoutInfo, [event.target.name]: event.target.value})
     }
 
+    const handleForm=()=>{
+        setWorkoutInfo({workoutName: "",
+        weight: "",
+        reps: "",})
+    }
+
     const handleSubmit = (event) =>{
         event.preventDefault()
         console.log(workoutInfo)
     }
+    
+
 
     return <>
     <h1>Add a new Workout</h1>
     <div className="formWorkout">
-    <form onSubmit={handleSubmit}>
+    <form id="workoutForm" onSubmit={handleSubmit}>
         <label>Workout</label>
         <input 
             type="text" 
             name="workoutName"
             value={workoutInfo.workoutName}
             onChange={handleChange}
+            placeholder="Bench Press"
             required>
         </input>
         <label>Weight</label>
         <input 
-            type="text" 
+            type="number" 
             name="weight"
             value={workoutInfo.weight}
             onChange={handleChange}
+            placeholder="220"
             required>
         </input>
         <label>How many Rep</label>
-        <input type="text" 
-        name="reps"
-        value={workoutInfo.reps}
-        onChange={handleChange}
-        required>
+        <input type="number" 
+            name="reps"
+            value={workoutInfo.reps}
+            onChange={handleChange}
+            placeholder="8"
+            required>
         </input>
-        <button>Create workout!</button>
+        <button onClick={handleForm}>Create workout!</button>
     </form>
     </div>
     </>
 }
 
-class Workouts{
-    constructor(workoutName, workoutCount, weight,
-        reps){
-            this.workoutName = workoutName
-            this.workoutCount = workoutCount
-            this.weight = weight
-            this.reps = reps
-        }
-}
+
 
 export default NewWorkout
