deb9563f1033e41867a5d29e8d0e4b895964bb42
ryan2625 <ryanfreas19@augustana.edu>
2023-03-21 15:06:38 -0500

    Changing workoutlog features

--- src/components/Dashboard/dashboardComponent/workoutLogCom/workoutLog.js
+++ src/components/Dashboard/dashboardComponent/workoutLogCom/workoutLog.js
@@ -3,9 +3,16 @@ import { Button, Icon, IconButton } from '@mui/material';
 import NewWorkout from './newWorkout';
 import React, { useEffect, useState } from "react";
 import './workoutLog.css'
+import WorkoutMonth from './logCalendar/workoutMonth';
+import WorkoutWeek from "./logCalendar/workoutWeek"
 function WorkoutLog() {
   const [added, addWorkout] = useState(false)
-    return(
+    return( <>
+    <div className='workoutLog-ctn'>
+    <div className="add-btns">
+      <button>Add Week</button>
+       <button>Add Month</button>
+    </div>
         <div className='log-container'>
         <IconButton id="add-btn" aria-label="Log a workout" 
         onClick={()=>addWorkout(!added)}>
@@ -13,6 +20,9 @@ function WorkoutLog() {
         </IconButton>
         {added && <NewWorkout/>}
         </div>
+        </div>
+        </>
+        
     )
 }
 
