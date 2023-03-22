e5225f49341f2cf95e12d587b0f15c0db451e091
eseyoum <ezanaseyoum20@augustana.edu>
2023-03-22 13:43:28 -0500

    edit navbar

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -35,7 +35,7 @@ export default function HomePage() {
                                 <div className="sec"></div>
                                 <div style={{marginBottom : '0'}}></div>
                             </div>
-                            <li><Link to='/about'>About</Link></li>
+                            <li><Link className='about' to='/about'>About</Link></li>
                             <li><a className='contact' href="#contact">Contact</a></li>
                             <li><Link className='log' to='/login'>Login</Link></li>
                         </ul>
