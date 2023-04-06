10090995a398ee96e56b4dde750c8f1ca50e55cf
ryan2625 <ryanfreas19@augustana.edu>
2023-04-05 19:53:45 -0500

    adding functionality

--- src/components/Dashboard/logWorkout/workoutDates/weeks/workoutWeekData.js
+++ src/components/Dashboard/logWorkout/workoutDates/weeks/workoutWeekData.js
@@ -1,8 +1,33 @@
 import React, {useEffect, useState} from 'react'
 export default function WorkoutWeekData(){
+
+    const [workoutWeekName, setWorkoutWeekName] =
+    useState([])
+
+    const [weekName, setWeekName] = useState("")
+
+    function handleClick(){
+        setWeekName(weekName)
+    }
+
+    function handleChange(event){
+        setWeekName(event.target.value)
+    }
+
+    function addWeek(){
+        const newWeek = <WorkoutWeekName name={weekName} key={workoutWeekName.length}/>
+        setWorkoutWeekName([...workoutWeekName, newWeek])
+    }
+
     return(<>
-        <input/>
+        <div>
+        <input onChange={handleChange}/>
         <h1>Hi</h1>
+        <button onClick={handleClick}>Submit Week Name</button>
+        </div>
         </>
     )
 }
+
+
+
---
+++ src/components/Dashboard/logWorkout/workoutDates/weeks/workoutWeekName.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react'
+export default function WorkoutWeekName(props) {
+
+    const[monthName, setMonthName] = useState("")
+    const[visibility, setVisibility]=useState(false)
+
+    function handleChange(event){
+        setMonthName(event.target.value)
+    }
+
+    function monthNamer(){
+        setVisibility(true)
+    }
+    
+    return(<>
+    <input onChange={handleChange} placeholder="Enter Month name"/>
+        <button onClick={monthNamer}>Submit Name</button>
+        {visibility && <WorkoutWeek name={monthName}/>}
+        </>)
+}
