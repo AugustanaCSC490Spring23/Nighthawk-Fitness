1c97d4b4256bc9718ad609d6730027fafc6cf763
menh161201 <minhnguyen161201@gmail.com>
2023-04-16 21:12:30 -0500

    Added plans

--- src/components/Dashboard/createWorkout/personalizedPlan/plans/Plans.js
+++ src/components/Dashboard/createWorkout/personalizedPlan/plans/Plans.js
@@ -174,4 +174,308 @@ const beginner = {
     }        
 }
 
+const intermediate = {
+    muscle_gain: {
+        five: {
+            workout: {
+                name: 'Push/Pull/Legs',
+                
+                schedule:
+                    [
+                        {
+                            session: 
+                                {
+                                    day: 1,
+                                        warm_up: 
+                                            [
+                                                {
+                                                    name: 'Jumping Jacks',
+                                                    instruction: '...',
+                                                    photoURL: '...'
+                                                }    
+                                            ]
+                                    ,
+                                    
+                                        main_workout: [
+                                            {
+                                                name: 'Bench Press'
+                                            },
+                                            {
+                                                name: 'Seated Dumbbell Shoulder Press'
+                                            },
+                                            {
+                                                name: 'Incline Dumbbell Press'
+                                            },
+                                            {
+                                                name: 'Side Lateral Raises'
+                                            },
+                                            {
+                                                name: 'Triceps Pressdowns'
+                                            },
+                                            {
+                                                name: 'Overhead Triceps Extension'
+                                            } 
+                                        ] 
+                                    ,
+                                    
+                                        cool_down:[
+                                            'Seated Toe Touches', 'Quad Stretch'
+                                        ]
+                                }
+                        },
+                        {
+                            session: 
+                                {
+                                    day: 2,
+                                        warm_up: 
+                                            [
+                                                'Jumping Jacks'
+                                            ]
+                                    ,
+                                    
+                                        main_workout: [
+                                            {
+                                                name: 'Bent-over Row'
+                                            },
+                                            {
+                                                name: 'Pull Ups'
+                                            },
+                                            {
+                                                name: 'Barbell Shrugs'
+                                            },
+                                            {
+                                                name: 'Face Pulls'
+                                            },
+                                            {
+                                                name: 'Barbell Curl'
+                                            },
+                                            {
+                                                name: 'Dumbbell Hammer Curl'
+                                            } 
+                                        ] 
+                                    ,
+                                    
+                                        cool_down:[
+                                            'Seated Toe Touches', 'Quad Stretch'
+                                        ]
+                                }
+                        },  
+                        {
+                            session: 
+                                {
+                                    day: 3,
+                                        warm_up: 
+                                            [
+                                                'Jumping Jacks'
+                                            ]
+                                    ,
+                                    
+                                        main_workout: [
+                                            {
+                                                name: 'Squats'
+                                            },
+                                            {
+                                                name: 'Romanian Deadlifts'
+                                            },
+                                            {
+                                                name: 'Leg Press'
+                                            },
+                                            {
+                                                name: 'Leg Curl'
+                                            },
+                                            {
+                                                name: 'Calf Raise'
+                                            },
+                                            {
+                                                name: 'Hanging Leg Raise'
+                                            } 
+                                        ] 
+                                    ,
+                                    
+                                        cool_down:[
+                                            'Seated Toe Touches', 'Quad Stretch'
+                                        ]
+                                }
+                        },
+                        'rest',
+                        {
+                            session: 
+                                {
+                                    day: 5,
+                                        warm_up: 
+                                            [
+                                                {
+                                                    name: 'Jumping Jacks',
+                                                    instruction: '...',
+                                                    photoURL: '...'
+                                                }    
+                                            ]
+                                    ,
+                                    
+                                        main_workout: [
+                                            {
+                                                name: 'Bench Press'
+                                            },
+                                            {
+                                                name: 'Seated Dumbbell Shoulder Press'
+                                            },
+                                            {
+                                                name: 'Incline Dumbbell Press'
+                                            },
+                                            {
+                                                name: 'Side Lateral Raises'
+                                            },
+                                            {
+                                                name: 'Triceps Pressdowns'
+                                            },
+                                            {
+                                                name: 'Overhead Triceps Extension'
+                                            } 
+                                        ] 
+                                    ,
+                                    
+                                        cool_down:[
+                                            'Seated Toe Touches', 'Quad Stretch'
+                                        ]
+                                }
+                        },
+                        {
+                            session: 
+                                {
+                                    day: 6,
+                                        warm_up: 
+                                            [
+                                                'Jumping Jacks'
+                                            ]
+                                    ,
+                                    
+                                        main_workout: [
+                                            {
+                                                name: 'Bent-over Row'
+                                            },
+                                            {
+                                                name: 'Pull Ups'
+                                            },
+                                            {
+                                                name: 'Barbell Shrugs'
+                                            },
+                                            {
+                                                name: 'Face Pulls'
+                                            },
+                                            {
+                                                name: 'Barbell Curl'
+                                            },
+                                            {
+                                                name: 'Dumbbell Hammer Curl'
+                                            } 
+                                        ] 
+                                    ,
+                                    
+                                        cool_down:[
+                                            'Seated Toe Touches', 'Quad Stretch'
+                                        ]
+                                }
+                        },
+                        'rest'
+                   ]
+            }
+        },
+        four_six: {
+            workout: {
+                name: 'Full Body Cardio',
+                
+                schedule:
+                    [
+                        {
+                            session: 
+                                {
+                                    day: 1,
+                                        warm_up: 
+                                            [
+                                                'Jumping Jacks'
+                                            ]
+                                    ,
+                                    
+                                        main_workout: [
+                                            {
+                                                name: '..',
+                                                photoURL: '..'
+                                            },
+
+                                            'Stationary Bike', 'Rowing Machine', 'Russian Twist', 'Treadmill' 
+                                        ] 
+                                    ,
+                                    
+                                        cool_down:[
+                                            'Seated Toe Touches', 'Quad Stretch'
+                                        ]
+                                }
+                        },
+                        'rest',
+                        {
+                            session: 
+                                {
+                                    day: 3,
+                                        warm_up: 
+                                            [
+                                                'Jumping Jacks'
+                                            ]
+                                    ,
+                                    
+                                        main_workout: [
+                                            'Stationary Bike', 'Rowing Machine', 'Russian Twist', 'Treadmill' 
+                                        ] 
+                                    ,
+                                    
+                                        cool_down:[
+                                            'Seated Toe Touches', 'Quad Stretch'
+                                        ]
+                                }
+                        }, 
+                        'rest'
+                        , {
+                            session: 
+                                {
+                                    day: 5,
+                                        warm_up: 
+                                            [
+                                                'Jumping Jacks'
+                                            ]
+                                    ,
+                                    
+                                        main_workout: [
+                                            'Stationary Bike', 'Rowing Machine', 'Russian Twist', 'Treadmill' 
+                                        ] 
+                                    ,
+                                    
+                                        cool_down:[
+                                            'Seated Toe Touches', 'Quad Stretch'
+                                        ]
+                                }
+                        }, {
+                            session: 
+                                {
+                                    day: 6,
+                                        warm_up: 
+                                            [
+                                                'Jumping Jacks'
+                                            ]
+                                    ,
+                                    
+                                        main_workout: [
+                                            'Stationary Bike', 'Rowing Machine', 'Russian Twist', 'Treadmill' 
+                                        ] 
+                                    ,
+                                    
+                                        cool_down:[
+                                            'Seated Toe Touches', 'Quad Stretch'
+                                        ]
+                                }
+                        }
+                   ]
+            }   
+        }   
+    }        
+}
+
 export default beginner;
