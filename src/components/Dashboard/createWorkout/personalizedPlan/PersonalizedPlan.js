0283454db35baf04eec18127ffb22cfde0b63431
menh161201 <minhnguyen161201@gmail.com>
2023-04-26 09:18:00 -0500

    Fixed personalized plan UI v1

--- src/components/Dashboard/createWorkout/personalizedPlan/PersonalizedPlan.js
+++ src/components/Dashboard/createWorkout/personalizedPlan/PersonalizedPlan.js
@@ -2,6 +2,8 @@ import React,  {useState} from 'react'
 import './personalizedplan.css'
 import { useNavigate } from 'react-router-dom';
 import {MdKeyboardArrowLeft} from 'react-icons/md'
+import {IoMdArrowDropup} from 'react-icons/io'
+import {RiEditCircleFill} from 'react-icons/ri'
 
 export default function PersonalizedPlan({userData}) {
   
@@ -15,23 +17,53 @@ export default function PersonalizedPlan({userData}) {
     
   }
 
-  function openType(index) {
-    setActiveType(index)
+  function openType(index1, index2) {
+    document.getElementById(index1).classList.toggle('active-type-item')
+    document.getElementById(index2).classList.toggle('active-type-icon')
   }
   
   function goBack()  {
     navigate(-1)
   }
 
+  function closeManage() {
+    document.getElementById('manage-tab').classList.remove('active-manage')
+  }
+
+  function openManage() {
+    document.getElementById('manage-tab').classList.add('active-manage')
+  }
+
   return (
     <div className="container">
+      <div className="manage-plan-tab" id='manage-tab'>
+        <div className="close" onClick={closeManage}>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="manage-plan-content">
+          <div className="manage-plan-item">
+            <h3> <span className='item-title'>Experience Level</span> <span className='item-details'>{userData.personal_preference.experience_level}</span><RiEditCircleFill className='edit-icon'/></h3>
+          </div>
+          <div className="manage-plan-item">
+            <h3> <span className='item-title'>Sessions per week</span> <span className='item-details'>{userData.personal_preference.workout_time}</span><RiEditCircleFill className='edit-icon'/></h3>
+          </div>
+          <div className="manage-plan-item">
+            <h3> <span className='item-title'>Duration</span> <span className='item-details'>4 weeks</span></h3>
+          </div>
+        </div>
+      </div>
       <div className='personal-plan'>
         <div className="back-btn" onClick={goBack}>
             <MdKeyboardArrowLeft />
         </div>
         <div className="personal-plan-title">
           <h2>{userData.plan.name}</h2>
-          <h3>Duration: <small>4 weeks</small></h3>
+          <div className="personal-plan-manage">
+            
+            <div className="manage-btn" onClick={openManage}>Manage Plan</div>
+          </div>
+          
         </div>
         
         <div className="tabs">
@@ -42,8 +74,8 @@ export default function PersonalizedPlan({userData}) {
             <div className={activeTab === index ? 'workout-day pressed' : 'workout-day'} onClick={() => openTab(index)}>
               <h3 className='week-day'>Day {item.session.day}</h3>
               <div className={activeTab === index ? 'tabs-content active-day' : 'tabs-content'} id={index}>
-                <h4 className='type' onClick={() => openType(index+10)}>warm up</h4>
-                <ul className={activeType === index+10 ? 'type-item active-type-item' : 'type-item'} id={index+10}>
+                <h4 className='type' onClick={() => openType(index+10, index+11)}>warm up <IoMdArrowDropup id={index+11} className='type-icon'/></h4>
+                <ul className='type-item' id={index+10}>
 
                   
                   {
@@ -64,8 +96,8 @@ export default function PersonalizedPlan({userData}) {
                   }
                 </ul>
 
-                <h4 className='type' onClick={() => openType(index+20)}>main workout</h4>  
-                <ul className={activeType === index+20 ? 'type-item active-type-item' : 'type-item'} id={index+20}>
+                <h4 className='type' onClick={() => openType(index+20, index+21)}>main workout <IoMdArrowDropup id={index+21} className='type-icon'/></h4>  
+                <ul className='type-item' id={index+20}>
                   
                   
                   {
@@ -86,8 +118,8 @@ export default function PersonalizedPlan({userData}) {
                   }
                 </ul>
 
-                <h4 className='type' onClick={() => openType(index+30)}>cool down  </h4>  
-                <ul className={activeType === index+30 ? 'type-item last active-type-item' : 'type-item last'} id={index+30}>
+                <h4 className='type' onClick={() => openType(index+30, index+31)}>cool down <IoMdArrowDropup id={index+31} className='type-icon'/></h4>  
+                <ul className='type-item' id={index+30}>
 
                   
                   {
